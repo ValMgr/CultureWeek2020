1783fc27015c6356481dc8c6f5ecb6c1b0c50a58
Valentin Magry <valou32720@gmail.com>
2019-11-18 12:43:57 +0100

    setup

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Semaine de la culture 2020</title>
+</head>
+<body>
+        <h1>Semaine de la culture 2020</h1>
+        <h2>Work in progress</h2>
+</body>
+</html>

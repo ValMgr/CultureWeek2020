30f4f93136c7c068ca67777177286a72035cc28e
ValMgz <valou32720@gmail.com>
2019-12-08 15:44:49 +0100

    optimize main.js

--- js/main.js
+++ js/main.js
@@ -10,6 +10,9 @@ document.documentElement.style.setProperty('--black', blackColor);
 document.documentElement.style.setProperty('--white', whiteColor);
 
 var daySelected = "LUNDI";
+el1 = document.getElementsByClassName("one")[0];
+el2 = document.getElementsByClassName("two")[0];
+el3 = document.getElementsByClassName("three")[0];
 
 
 function DaySelect(id){
@@ -22,57 +25,38 @@ function DaySelect(id){
     days[id].classList.add("daySelected");
     daySelected = days[id].innerHTML.substring(3).split('<')[0];;
     //console.log(daySelected);
-    switchMenu();
-    
+    switchMenu(el1, 'up');
+    switchMenu(el2, 'down')
+    switchMenu(el3, 'up')
 }
 
-function switchMenu(){
-    el1 = document.getElementsByClassName("one")[0];
-    el2 = document.getElementsByClassName("two")[0];
-    el3 = document.getElementsByClassName("three")[0];
 
 
-    el1.classList.remove('slideDown');
-    el1.classList.add('slideUp2');
-        setTimeout(function () {
-            Menu(daySelected)
-            el1.classList.remove('slideUp2');
-            el1.classList.add('slideDown');
-        }, 300);
+function switchMenu(el, way){
 
-    el2.classList.remove('slideUp');
-    el2.classList.add('slideDown2');
-        setTimeout(function () {
-            Menu(daySelected)
-            el2.classList.remove('slideDown2');
-            el2.classList.add('slideUp');
-        }, 300);
-
-    el3.classList.remove('slideDown');
-    el3.classList.add('slideUp2');
-        setTimeout(function () {
-            Menu(daySelected)
-            el3.classList.remove('slideUp2');
-            el3.classList.add('slideDown');
-        }, 300);
-    
+    if(way == 'up'){
+        el.classList.remove('slideDown');
+        el.classList.add('slideUp2');
+            setTimeout(function () {
+                Menu(daySelected, el)
+                el.classList.remove('slideUp2');
+                el.classList.add('slideDown');
+            }, 300);
+    }
+    if(way == 'down'){
+        el.classList.remove('slideUp');
+        el.classList.add('slideDown2');
+            setTimeout(function () {
+                Menu(daySelected, el)
+                el.classList.remove('slideDown2');
+                el.classList.add('slideUp');
+            }, 300);
+    }    
 }
 
-function Menu(day){
-    el1 = document.getElementsByClassName("one")[0];
-    Array.from(el1.children).forEach(element => {
-        if(element.getAttribute("day") == day) element.style.display = "unset";
-        if(element.getAttribute("day") != day)  element.style.display = "none";
-    });
-
-    el2 = document.getElementsByClassName("two")[0];
-    Array.from(el2.children).forEach(element => {
-        if(element.getAttribute("day") == day) element.style.display = "unset";
-        if(element.getAttribute("day") != day)  element.style.display = "none";
-    });
-
-    el3 = document.getElementsByClassName("three")[0];
-    Array.from(el3.children).forEach(element => {
+function Menu(day, el){
+    el = document.getElementsByClassName("one")[0];
+    Array.from(el.children).forEach(element => {
         if(element.getAttribute("day") == day) element.style.display = "unset";
         if(element.getAttribute("day") != day)  element.style.display = "none";
     });
